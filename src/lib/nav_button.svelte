c46bba4a1423f96973d1615d2abdad8c720beeca
Dusan Figala <dusanfigala@protonmail.com>
2023-10-26 22:44:17 +0200

    Update navbar

---
+++ src/lib/nav_button.svelte
@@ -0,0 +1,6 @@
+<button
+	class="py-1 px-4 rounded-md text-slate-700 font-medium
+ hover:bg-slate-200 hover:text-slate-900"
+>
+	<slot />
+</button>
--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,1 +1,15 @@
-<p>navbar</p>
+<script>
+	import { NavButton } from '$lib';
+</script>
+
+<nav class="flex w-full justify-between items-center px-4 py-2 shadow-md">
+	<img src="" alt="Logo" class="" />
+	<div class="">
+		<NavButton>Služby</NavButton>
+		<NavButton>O mně</NavButton>
+		<NavButton>Ceník</NavButton>
+		<NavButton>Akutní problémy</NavButton>
+		<NavButton>Etický kodex</NavButton>
+		<NavButton>Kontakt</NavButton>
+	</div>
+</nav>
