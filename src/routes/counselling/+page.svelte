441b57e07ce0e3c091199898f48c71dade888bdc
Dusan Figala <dusanfigala@protonmail.com>
2023-10-28 21:07:45 +0200

    Add service pages

---
+++ src/routes/counselling/+page.svelte
@@ -0,0 +1,29 @@
+<h1>Psychologické poradenství</h1>
+<p>
+	Psychologické poradenství je konzultace a činnost zaměřena na překonání aktuálních psychických
+	problémů klientů nebo konfliktních situací za pomocí psychologickcýh metod a prostředků. Může se
+	jednat o oblast kariéry (volba a změna povolání), rodiny, manželských a partnerských vztahů,
+	školní a výchovnou nebo o oblast vztahu k sobě sama a rozvoje osobnosti.
+</p>
+
+<h2>Co konkrétně do této oblasti může patřit?</h2>
+
+<ul>
+	<li>aktuální výskyt úzkostí před zkouškou nebo prezentací</li>
+	<li>náročná situace v partnerském nebo manželském vztahu</li>
+	<li>příprava na roli rodiče nebo prarodiče</li>
+	<li>volba výchovného stylu</li>
+	<li>adaptace na situaci odchodu do důchodu</li>
+	<li>volba vysoké školy</li>
+	<li>změna povolání v produktivním věku</li>
+	<li>pracovní zátěž</li>
+	<li>konfliktní situace</li>
+	<li>otázky sebehodnoty a spirituality</li>
+	<li>seberozvojová témata (např. rozvíjení soft skills)</li>
+</ul>
+<p>
+	Moje cílová skupina jsou dospělí lidé. Pracuji tedy s klienty, kteří již dosáhli plnoletosti.
+	Výjimky pouze ve velice akutních případech po telefonické domluvě. Nevěnuji se rodinné a
+	partnerské sféře, nepracuji s dětmi a s osobami se závažnými psychickými poruchami. Pracuji vždy
+	pouze s jednotlivci.
+</p>
