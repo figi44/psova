15eb39e2cc1286d9f73541f77a80bca6bb6cb61a
Dusan Figala <dusanfigala@protonmail.com>
2023-10-30 20:22:28 +0100

    Rename pages in czech

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,42 +7,42 @@
 			title: 'Psychologické poradenství',
 			text: 'Konzultace a činnost zaměřena na překonání aktuálních psychických problémů nebo konfliktních situací za pomocí psychologickcýh metod a prostředků.',
 			linkText: '',
-			linkHref: 'counselling',
+			linkHref: 'poradenstvi',
 			dotColor: 'bg-custom-pink'
 		},
 		{
 			title: 'Koučink',
 			text: 'Proces, kterým na základě vhodně a efektivně zvolených otázek kouč provází klienta ke stanovenému cíli.',
 			linkText: '',
-			linkHref: 'coaching',
+			linkHref: 'koucink',
 			dotColor: 'bg-custom-orange'
 		},
 		{
 			title: 'Psychohygiena',
 			text: 'Upevňování duševního zdraví a zvyšování odolnosti klienta vůči široké škále škodlivých vlivů.',
 			linkText: '',
-			linkHref: '',
+			linkHref: 'psychohygiena',
 			dotColor: 'bg-custom-yellow'
 		},
 		{
 			title: 'Psychodiagnostika',
 			text: 'Zjišťování a měření duševních vlastností a stavů dle zadání klienta.',
 			linkText: '',
-			linkHref: '',
+			linkHref: 'psychodiagnostika',
 			dotColor: 'bg-custom-green'
 		},
 		{
-			title: 'Edukace v oblasti duševního zdraví',
+			title: 'Podpůrné skupiny',
 			text: 'Praktické metody a techniky pro všední den, přednášky pro školy i jednotlivce.',
 			linkText: '',
-			linkHref: '',
+			linkHref: 'skupiny',
 			dotColor: 'bg-custom-blue'
 		},
 		{
 			title: 'Firemní aktivity',
 			text: 'Individuálně nastavené balíčky na míru firmám a kolektivům pro podporu a prevenci duševního zdraví zaměstnanců.',
 			linkText: '',
-			linkHref: '',
+			linkHref: 'firmy',
 			dotColor: 'bg-custom-purple'
 		}
 	];
