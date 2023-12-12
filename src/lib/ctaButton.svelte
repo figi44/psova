227bbfa725ed14f4b7e2830a198c7dcd5d4f3982
Dusan Figala <dusanfigala@protonmail.com>
2023-12-12 21:03:08 +0100

    Navigate CTA button

--- src/lib/ctaButton.svelte
+++ src/lib/ctaButton.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	export let title: string;
 </script>
 
@@ -6,6 +7,9 @@
 	class="py-[6px] px-[14px] rounded-[30px] text-white text-xl font-bold bg-xpink border-xpink border-2
 			 hover:border-black hover:text-black transition-all ease-in
            "
+	on:click={() => {
+		goto('/kontakt');
+	}}
 >
 	{title}
 </button>
