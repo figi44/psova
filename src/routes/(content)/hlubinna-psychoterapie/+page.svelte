a72da379a1ef014a3b3af553f2be786881e2aceb
Dusan Figala <dusanfigala@protonmail.com>
2026-08-16 19:42:54 +0200

    Add male therapist referral

--- src/routes/(content)/hlubinna-psychoterapie/+page.svelte
+++ src/routes/(content)/hlubinna-psychoterapie/+page.svelte
@@ -51,4 +51,22 @@
 	Nevěnuji se rodinné a partnerské sféře, nepracuji s dětmi. Pracuji vždy pouze s jednotlivci.
 </Paragraph>
 
+<aside class="mt-14" aria-label="Doporučení kolegy">
+	<Paragraph>
+		<span class="font-bold">Preferovali byste raději terapeuta - muže?</span>
+		Ve stejné terapeutovně pracuje také můj kolega
+		<a
+			href="https://www.rubes-terapie.cz/"
+			class="inline-flex items-center gap-[6px] font-bold text-xlavender transition-colors hover:text-xpink"
+		>
+			Jakub Rubeš
+			<svg viewBox="0 0 21 9" fill="none" class="mt-[3px] w-5 stroke-current" aria-hidden="true">
+				<path
+					d="M20.3536 4.85355C20.5488 4.65829 20.5488 4.34171 20.3536 4.14645L17.1716 0.964466C16.9763 0.769204 16.6597 0.769204 16.4645 0.964466C16.2692 1.15973 16.2692 1.47631 16.4645 1.67157L19.2929 4.5L16.4645 7.32843C16.2692 7.52369 16.2692 7.84027 16.4645 8.03553C16.6597 8.2308 16.9763 8.2308 17.1716 8.03553L20.3536 4.85355ZM0 5H20V4H0V5Z"
+				/>
+			</svg>
+		</a>.
+	</Paragraph>
+</aside>
+
 <ServiceCTA duration="50 nebo 75 minut" price="od 1 000 Kč" />
